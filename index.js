aa21c4ba02500b8e23c069d8b017662813802081
Parvez-hasan <parvez7788@gmail.com>
2025-07-20 13:29:04 +0600

    fangstion and js add

---
+++ index.js
@@ -0,0 +1,8 @@
+const dataload= ()=>{}
+
+fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+.then((res)=>console.log(res))
+.catch((err)=>console.log(err))
+
+
+const dataDisplay= ()=>{}
