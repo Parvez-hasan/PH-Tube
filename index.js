e466ab4b48066b913b02c038a272a51e53701e45
Parvez-hasan <parvez7788@gmail.com>
2025-08-03 13:22:57 +0600

    cart img and title add

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
 .catch((err)=> console.log(err))
 
 }
+// videolond 
+const videolond = ()=>{
+
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+.then((res)=> res.json())
+.then((data)=> displayVideo (data.videos))
+.catch((err)=> console.log(err))
+
+}
+videolond()
+
+
+
 
 const dataDisplay= (categories)=>{
     
@@ -13,9 +26,7 @@ const btndiv = document.getElementById("buttondiv");
 
 categories.forEach(item => {
 
-    console.log(item.category);
- 
-
+console.log(item.category);
 
 const button= document.createElement('button')
 button.classList='btn'
@@ -26,9 +37,45 @@ btndiv.append(button)
 });
 
 }
-
-
 dataload()
+              
+
+// videolond 
+
+const displayVideo = (videoPaice)=>{
+  const videodiv = document.createElement('div')
+videodiv.classList=`flex justify-center gap-7 flex-wrap `
+
+console.log(videoPaice);
+
+videoPaice.forEach((item)=>{
+
+// console.log(item);
+const cart= document.createElement('div')
+cart.innerHTML=`
+<div class="card bg-base-100 w-96 shadow-sm">
+  <figure class="h-48">
+    <img
+      src="${item.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${item.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+`;
+
+videodiv.append(cart)
+
+});
+
+document.body.append(videodiv)
+
+}
 
 
 
